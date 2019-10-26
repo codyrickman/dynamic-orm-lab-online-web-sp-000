c6000f69aba4c645cd3a45a8e8d2802cf5e9b7c8
Cody Rickman <cody@creativemindstudios.com>
2019-10-26 02:03:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,4 +8,11 @@ class Student < InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    sql = <<-SQL
+            PRAGMA table_info(?)
+            SQL
+    DB[:conn].execute(sql, self.table_name)
+  end
+
 end
